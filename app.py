77102db30accd091d5d4137031d394e6321f96bb
krish soni <krishsoni29112004@gmail.com>
2026-04-25 11:35:37 +0530

    columns

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import streamlit as st
 import pandas as pd
+import joblib #for unpickling
+
+model = joblib.load(KNN_HEART.pkl)
+scaler = joblib.load(KNN_HEART.pkl)
+expected_columns = joblib.load(KNN_HEART.pkl)
+
